cda23e44647a88bffadaff30c9bfe4280e07f95a
Zin <znajer@gmail.com>
2019-04-14 16:03:59 +0200

    Test

--- src/main/java/SheetReaderImplementation.java
+++ src/main/java/SheetReaderImplementation.java
@@ -1,3 +1,4 @@
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -9,6 +10,7 @@ public class SheetReaderImplementation implements SheetReader{
 
     @Override
     public TestEvaluation readSheet(File file) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat imageArray;
         imageArray= Imgcodecs.imread(file.getPath(),Imgcodecs.IMREAD_COLOR);
         HighGui.namedWindow("Display",HighGui.WINDOW_NORMAL);
