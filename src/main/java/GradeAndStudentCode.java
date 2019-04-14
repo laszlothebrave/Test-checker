0f2ff682f2ae882ccc19d7afb64a0a7065f17c8f
Golden97 <andreaplu07@gmail.com>
2019-04-14 17:07:24 +0200

    funkcja assignGrade zwraca teraz GradeAndStudentCode.  GradeAndStudentCode ma teraz funkcja toString().

--- src/main/java/GradeAndStudentCode.java
+++ src/main/java/GradeAndStudentCode.java
@@ -7,4 +7,27 @@ public class GradeAndStudentCode {
     public void setGrade(Grade grade){
         this.grade=grade;
     }
+
+    public String toString(){
+        StringBuilder codeAndGrade= new StringBuilder();
+        codeAndGrade.append(studentCode);
+        codeAndGrade.append(" dear Student your grade is: ");
+        switch(grade){
+            case A:
+                codeAndGrade.append("A (5)");
+            case B:
+                codeAndGrade.append("B (4.5)");
+            case C:
+                codeAndGrade.append("C (4)");
+            case D:
+                codeAndGrade.append("D (3.5)");
+            case E:
+                codeAndGrade.append("E (3)");
+            case F:
+                codeAndGrade.append("F (2)");
+
+        }
+
+        return codeAndGrade.toString();
+    }
 }
