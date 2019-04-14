14e4c38d4801028f87dabe231f1fb411d30e518a
laszlothebrave <leszekmilasz@gmail.com>
2019-04-14 14:47:12 +0200

    adding documents

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -1,16 +1,28 @@
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class ConsoleApp {
+    static SheetReader sheetReader;
+    static GradeAssigner gradeAssigner;
+    static path
+    static Path[] paths;
+    static TestEvaluation correctAnswers;
+    static ArrayList<TestEvaluation> tests;
+
     public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("mat = " + mat.dump());
+        createPaths(args);
+        readCorrectAnswersSheet();
+        readAtLeastOneStudentSheet();
+        printResults();
+    }
+
+    private static void createPaths(String[] args) {
+
     }
 
     private static void readCorrectAnswersSheet(){
-
+        correctAnswers = sheetReader.readSheet(paths[0]);
+        if (correctAnswers == null) throw new CantReadCorrectAnswers();
     }
 
     private static void readAtLeastOneStudentSheet(){
@@ -20,4 +32,14 @@ public class ConsoleApp {
     private static void printResults(){
 
     }
+
+
 }
+
+
+//import org.opencv.core.Core;
+//import org.opencv.core.CvType;
+//import org.opencv.core.Mat;
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+//        System.out.println("mat = " + mat.dump());
