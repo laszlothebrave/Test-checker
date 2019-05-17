2d307529981e30b9386c8b0449d59e6810d385d3
Golden97 <andreaplu07@gmail.com>
2019-05-17 16:39:45 +0200

    break na switch

--- src/main/java/GradeAndStudentCode.java
+++ src/main/java/GradeAndStudentCode.java
@@ -15,16 +15,22 @@ public class GradeAndStudentCode {
         switch(grade){
             case A:
                 codeAndGrade.append("A (5)");
+                break;
             case B:
                 codeAndGrade.append("B (4.5)");
+                break;
             case C:
                 codeAndGrade.append("C (4)");
+                break;
             case D:
                 codeAndGrade.append("D (3.5)");
+                break;
             case E:
                 codeAndGrade.append("E (3)");
+                break;
             case F:
                 codeAndGrade.append("F (2)");
+                break;
 
         }
 
